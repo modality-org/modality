07f3a4e6767e2e505a3c07522a3058b189ee5d27
Foy Savas <foy@sav.as>
2025-02-08 23:41:01 -0500

    use js cli for now

--- examples/network/02-run-devnet2/01-run-node1.sh
+++ examples/network/02-run-devnet2/01-run-node1.sh
@@ -2,4 +2,4 @@
 cd $(dirname -- "$0")
 set -x
 
-modality net run-node --config ../../../fixtures/network-node-configs/devnet2/node1.json --enable-consensus
+modality-js net run-node --config ../../../fixtures/network-node-configs/devnet2/node1.json --enable-consensus
