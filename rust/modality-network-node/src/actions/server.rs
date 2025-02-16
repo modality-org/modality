c476f0fcd6d57f61081bca552dd7ce035258ef13
Foy Savas <foy@sav.as>
2025-02-16 09:41:50 -0500

    refactor

--- rust/modality-network-node/src/actions/server.rs
+++ rust/modality-network-node/src/actions/server.rs
@@ -17,17 +17,6 @@ use std::sync::Arc;
 use crate::gossip;
 
 pub async fn run(node: &mut Node) -> Result<()> {
-    let running = Arc::new(AtomicBool::new(true));
-    let running_shared: Arc<AtomicBool> = running.clone();
-    ctrlc::set_handler(move || {
-        println!("Received Ctrl-C, shutting down...");
-        running_shared.store(false, Ordering::SeqCst);
-    }).expect("Error setting Ctrl-C handler");
-
-
-    let tick_interval: Duration = Duration::from_secs(15);
-    let mut tick = futures_timer::Delay::new(tick_interval);
-
     gossip::add_sequencer_event_listeners(node).await?;
 
     node.run().await?;
