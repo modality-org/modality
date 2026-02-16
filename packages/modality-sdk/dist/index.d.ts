7351fb1884e57999f853d1363c9e5a6645cd7347
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-02-16 18:18:03 +0000

    chore: add .gitignore, remove tracked node_modules and dist

--- packages/modality-sdk/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { generateIdentity, identityFromPrivateKey, sign, verify, signJSON, verifyJSON, type Identity, } from './identity.js';
-export { createContract, verifyGenesis, createCommit, signCommit, verifyCommit, commitHash, hashJSON, type ContractGenesis, type Commit, type CommitAction, } from './contract.js';
-export { generateCard, type ContractCard } from './card.js';
-export { escrow, taskDelegation, dataExchange, type IntentTemplate, type EscrowTerms, type TaskDelegationTerms, type DataExchangeTerms, } from './templates.js';
