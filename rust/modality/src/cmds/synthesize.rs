ba4631ec066d2f6c564a54891c290d9f89f7f008
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-06-14 15:19:59 +0000

    synthesis: list agent turn-taking example

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -424,6 +424,7 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"[<+RELEASE>] true -> (eventually(<+DEPOSIT> true) & eventually(<+DELIVER> true))"#,
             r#"[+RELEASE] true -> (eventually([<+DEPOSIT>] true) & eventually([<+DELIVER>] true))"#,
             r#"[<+RELEASE>] true -> (eventually([<+DEPOSIT>] true) & eventually([<+DELIVER>] true))"#,
+            r#"always([+AGENT_A_TURN] true -> eventually(<+AGENT_B_TURN> true))"#,
         ],
     },
     FormulaExampleGroup {
