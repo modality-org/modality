ddab02c1c52d6d27b6012f343cdfc5333d6978d1
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-06-25 05:05:48 +0000

    synthesis: list committed recursion example

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -469,6 +469,7 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"[<+RELEASE>] true -> (eventually([<+DEPOSIT>] true) & eventually([<+DELIVER>] true))"#,
             r#"always([+AGENT_A_TURN] true -> eventually(<+AGENT_B_TURN> true))"#,
             r#"always([+AGENT_B_TURN] true -> eventually(<+AGENT_A_TURN> true))"#,
+            r#"lfp(X, ([<+APPROVE>] true) | [<>]X)"#,
         ],
     },
     FormulaExampleGroup {
