f17290c3ae6f7a336489db2c62cdff7ec46d36d5
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-06-28 18:58:25 +0000

    synthesis: add decision launch examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -1202,6 +1202,9 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+REQUEST_DECISION_ACCEPTANCE_REVIEW] true -> eventually(<+MEASURE_DECISION_ACCEPTANCE> true))"#,
             r#"always([+MEASURE_DECISION_ACCEPTANCE] true -> eventually(<+APPROVE_DECISION_ACCEPTANCE> true))"#,
             r#"always([+APPROVE_DECISION_ACCEPTANCE] true -> eventually(<+PUBLISH_DECISION_ACCEPTANCE> true))"#,
+            r#"always([+REQUEST_DECISION_LAUNCH_REVIEW] true -> eventually(<+MEASURE_DECISION_LAUNCH> true))"#,
+            r#"always([+MEASURE_DECISION_LAUNCH] true -> eventually(<+APPROVE_DECISION_LAUNCH> true))"#,
+            r#"always([+APPROVE_DECISION_LAUNCH] true -> eventually(<+PUBLISH_DECISION_LAUNCH> true))"#,
             r#"[+RELEASE] true -> eventually((<+DEPOSIT> true & <+DELIVER> true))"#,
             r#"[+RELEASE] true -> eventually(([<+DEPOSIT>] true & [<+DELIVER>] true))"#,
             r#"[+RELEASE] true -> (eventually(<+DEPOSIT> true) & eventually(<+DELIVER> true))"#,
@@ -15801,6 +15804,24 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "DecisionAcceptance",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_decision_launch_ordering_prompt_examples() {
+        let formulas = parse_formula_strings(&[
+            "always([+REQUEST_DECISION_LAUNCH_REVIEW] true -> eventually(<+MEASURE_DECISION_LAUNCH> true))"
+                .to_string(),
+            "always([+MEASURE_DECISION_LAUNCH] true -> eventually(<+APPROVE_DECISION_LAUNCH> true))"
+                .to_string(),
+            "always([+APPROVE_DECISION_LAUNCH] true -> eventually(<+PUBLISH_DECISION_LAUNCH> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "DecisionLaunch",
             &formulas,
         );
 
