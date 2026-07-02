cf432fd3b5c078ebabca0cc419f8eda33cd66b2e
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-07-02 15:19:09 +0000

    synthesis: add support key agreement message examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -697,6 +697,8 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+PURGE_SUPPORT_KEY_DISTRIBUTION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"always([+SUPPORT_KEY_EXCHANGE_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_EXCHANGE_MESSAGES> true | <+REVOKE_KEY_EXCHANGE_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
             r#"always([+PURGE_SUPPORT_KEY_EXCHANGE_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
+            r#"always([+SUPPORT_KEY_AGREEMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_AGREEMENT_MESSAGES> true | <+REVOKE_KEY_AGREEMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
+            r#"always([+PURGE_SUPPORT_KEY_AGREEMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"next(<+APPROVE> true)"#,
             r#"next((<+APPROVE> true | [<+REJECT>] true))"#,
             r#"<+WAIT> true until <+APPROVE> true"#,
@@ -6430,6 +6432,18 @@ F2: formula generated_2 {
         ));
         assert!(output.contains(
             "always([+PURGE_SUPPORT_KEY_EXCHANGE_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+        ));
+    }
+
+    #[test]
+    fn synthesis_list_includes_support_key_agreement_message_secret_leak_prompt_examples() {
+        let output = synthesis_list_text();
+
+        assert!(output.contains(
+            "always([+SUPPORT_KEY_AGREEMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_AGREEMENT_MESSAGES> true | <+REVOKE_KEY_AGREEMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"
+        ));
+        assert!(output.contains(
+            "always([+PURGE_SUPPORT_KEY_AGREEMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
         ));
     }
 
@@ -18012,6 +18026,23 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "SupportKeyExchangeMessageSecretLeak",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_support_key_agreement_message_secret_leak_prompt_examples()
+    {
+        let formulas = parse_formula_strings(&[
+            "always([+SUPPORT_KEY_AGREEMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_AGREEMENT_MESSAGES> true | <+REVOKE_KEY_AGREEMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"
+                .to_string(),
+            "always([+PURGE_SUPPORT_KEY_AGREEMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "SupportKeyAgreementMessageSecretLeak",
             &formulas,
         );
 
