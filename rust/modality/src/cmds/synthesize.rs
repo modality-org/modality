89a2a7a37998dd92f511064e5225c1df9e4d908f
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-07-02 00:13:01 +0000

    synthesis: add support rtf message examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -591,6 +591,8 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+PURGE_SUPPORT_MARKDOWN_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"always([+SUPPORT_HTML_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_HTML_MESSAGES> true | <+REVOKE_HTML_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
             r#"always([+PURGE_SUPPORT_HTML_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
+            r#"always([+SUPPORT_RTF_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_RTF_MESSAGES> true | <+REVOKE_RTF_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
+            r#"always([+PURGE_SUPPORT_RTF_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"next(<+APPROVE> true)"#,
             r#"next((<+APPROVE> true | [<+REJECT>] true))"#,
             r#"<+WAIT> true until <+APPROVE> true"#,
@@ -5688,6 +5690,18 @@ F2: formula generated_2 {
         ));
         assert!(output.contains(
             "always([+PURGE_SUPPORT_HTML_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+        ));
+    }
+
+    #[test]
+    fn synthesis_list_includes_support_rtf_message_secret_leak_prompt_examples() {
+        let output = synthesis_list_text();
+
+        assert!(output.contains(
+            "always([+SUPPORT_RTF_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_RTF_MESSAGES> true | <+REVOKE_RTF_MESSAGE_EXPOSED_CREDENTIALS> true))"
+        ));
+        assert!(output.contains(
+            "always([+PURGE_SUPPORT_RTF_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
         ));
     }
 
@@ -16412,6 +16426,22 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "SupportHtmlMessageSecretLeak",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_support_rtf_message_secret_leak_prompt_examples() {
+        let formulas = parse_formula_strings(&[
+            "always([+SUPPORT_RTF_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_RTF_MESSAGES> true | <+REVOKE_RTF_MESSAGE_EXPOSED_CREDENTIALS> true))"
+                .to_string(),
+            "always([+PURGE_SUPPORT_RTF_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "SupportRtfMessageSecretLeak",
             &formulas,
         );
 
