32a309b6e97650d24fa5989908379e8583bdefc1
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-07-03 01:21:42 +0000

    synthesis: add support key alignment message examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -767,6 +767,8 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+PURGE_SUPPORT_KEY_COORDINATION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"always([+SUPPORT_KEY_SYNCHRONIZATION_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_SYNCHRONIZATION_MESSAGES> true | <+REVOKE_KEY_SYNCHRONIZATION_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
             r#"always([+PURGE_SUPPORT_KEY_SYNCHRONIZATION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
+            r#"always([+SUPPORT_KEY_ALIGNMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_ALIGNMENT_MESSAGES> true | <+REVOKE_KEY_ALIGNMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
+            r#"always([+PURGE_SUPPORT_KEY_ALIGNMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"next(<+APPROVE> true)"#,
             r#"next((<+APPROVE> true | [<+REJECT>] true))"#,
             r#"<+WAIT> true until <+APPROVE> true"#,
@@ -6920,6 +6922,18 @@ F2: formula generated_2 {
         ));
         assert!(output.contains(
             "always([+PURGE_SUPPORT_KEY_SYNCHRONIZATION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+        ));
+    }
+
+    #[test]
+    fn synthesis_list_includes_support_key_alignment_message_secret_leak_prompt_examples() {
+        let output = synthesis_list_text();
+
+        assert!(output.contains(
+            "always([+SUPPORT_KEY_ALIGNMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_ALIGNMENT_MESSAGES> true | <+REVOKE_KEY_ALIGNMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"
+        ));
+        assert!(output.contains(
+            "always([+PURGE_SUPPORT_KEY_ALIGNMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
         ));
     }
 
@@ -19088,6 +19102,23 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "SupportKeySynchronizationMessageSecretLeak",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_support_key_alignment_message_secret_leak_prompt_examples()
+    {
+        let formulas = parse_formula_strings(&[
+            "always([+SUPPORT_KEY_ALIGNMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_ALIGNMENT_MESSAGES> true | <+REVOKE_KEY_ALIGNMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"
+                .to_string(),
+            "always([+PURGE_SUPPORT_KEY_ALIGNMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "SupportKeyAlignmentMessageSecretLeak",
             &formulas,
         );
 
