eccce9a479d96f246a5162bad95dc63809ff8c03
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-07-03 12:10:50 +0000

    synthesis: add support key regeneration message examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -839,6 +839,8 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+PURGE_SUPPORT_KEY_RESET_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"always([+SUPPORT_KEY_REPLACEMENT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_REPLACEMENT_MESSAGES> true | <+REVOKE_KEY_REPLACEMENT_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
             r#"always([+PURGE_SUPPORT_KEY_REPLACEMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
+            r#"always([+SUPPORT_KEY_REGENERATION_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_REGENERATION_MESSAGES> true | <+REVOKE_KEY_REGENERATION_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
+            r#"always([+PURGE_SUPPORT_KEY_REGENERATION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"next(<+APPROVE> true)"#,
             r#"next((<+APPROVE> true | [<+REJECT>] true))"#,
             r#"<+WAIT> true until <+APPROVE> true"#,
@@ -7424,6 +7426,18 @@ F2: formula generated_2 {
         ));
         assert!(output.contains(
             "always([+PURGE_SUPPORT_KEY_REPLACEMENT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+        ));
+    }
+
+    #[test]
+    fn synthesis_list_includes_support_key_regeneration_message_secret_leak_prompt_examples() {
+        let output = synthesis_list_text();
+
+        assert!(output.contains(
+            "always([+SUPPORT_KEY_REGENERATION_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_REGENERATION_MESSAGES> true | <+REVOKE_KEY_REGENERATION_MESSAGE_EXPOSED_CREDENTIALS> true))"
+        ));
+        assert!(output.contains(
+            "always([+PURGE_SUPPORT_KEY_REGENERATION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
         ));
     }
 
@@ -20195,6 +20209,23 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "SupportKeyReplacementMessageSecretLeak",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_support_key_regeneration_message_secret_leak_prompt_examples(
+    ) {
+        let formulas = parse_formula_strings(&[
+            "always([+SUPPORT_KEY_REGENERATION_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_REGENERATION_MESSAGES> true | <+REVOKE_KEY_REGENERATION_MESSAGE_EXPOSED_CREDENTIALS> true))"
+                .to_string(),
+            "always([+PURGE_SUPPORT_KEY_REGENERATION_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "SupportKeyRegenerationMessageSecretLeak",
             &formulas,
         );
 
