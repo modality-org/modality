01757e2c905d160d7275c65b2325c6470f6b293e
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-07-03 04:23:45 +0000

    synthesis: add support key receipt message examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -787,6 +787,8 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+PURGE_SUPPORT_KEY_DISPATCH_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"always([+SUPPORT_KEY_DELIVERY_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_DELIVERY_MESSAGES> true | <+REVOKE_KEY_DELIVERY_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
             r#"always([+PURGE_SUPPORT_KEY_DELIVERY_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
+            r#"always([+SUPPORT_KEY_RECEIPT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_RECEIPT_MESSAGES> true | <+REVOKE_KEY_RECEIPT_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
+            r#"always([+PURGE_SUPPORT_KEY_RECEIPT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"next(<+APPROVE> true)"#,
             r#"next((<+APPROVE> true | [<+REJECT>] true))"#,
             r#"<+WAIT> true until <+APPROVE> true"#,
@@ -7060,6 +7062,18 @@ F2: formula generated_2 {
         ));
         assert!(output.contains(
             "always([+PURGE_SUPPORT_KEY_DELIVERY_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+        ));
+    }
+
+    #[test]
+    fn synthesis_list_includes_support_key_receipt_message_secret_leak_prompt_examples() {
+        let output = synthesis_list_text();
+
+        assert!(output.contains(
+            "always([+SUPPORT_KEY_RECEIPT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_RECEIPT_MESSAGES> true | <+REVOKE_KEY_RECEIPT_MESSAGE_EXPOSED_CREDENTIALS> true))"
+        ));
+        assert!(output.contains(
+            "always([+PURGE_SUPPORT_KEY_RECEIPT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
         ));
     }
 
@@ -19397,6 +19411,22 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "SupportKeyDeliveryMessageSecretLeak",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_support_key_receipt_message_secret_leak_prompt_examples() {
+        let formulas = parse_formula_strings(&[
+            "always([+SUPPORT_KEY_RECEIPT_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_RECEIPT_MESSAGES> true | <+REVOKE_KEY_RECEIPT_MESSAGE_EXPOSED_CREDENTIALS> true))"
+                .to_string(),
+            "always([+PURGE_SUPPORT_KEY_RECEIPT_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "SupportKeyReceiptMessageSecretLeak",
             &formulas,
         );
 
