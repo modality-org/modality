1bc9ca05e7402352c15b876e760358b56f63c5ab
Gerold Steiner <geroldsteiner67@users.noreply.github.com>
2026-07-04 02:16:50 +0000

    synthesis: add support key interweaving message examples

--- rust/modality/src/cmds/synthesize.rs
+++ rust/modality/src/cmds/synthesize.rs
@@ -939,6 +939,8 @@ const FORMULA_EXAMPLE_GROUPS: &[FormulaExampleGroup] = &[
             r#"always([+PURGE_SUPPORT_KEY_COMMINGLING_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"always([+SUPPORT_KEY_INTERMIXING_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_INTERMIXING_MESSAGES> true | <+REVOKE_KEY_INTERMIXING_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
             r#"always([+PURGE_SUPPORT_KEY_INTERMIXING_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
+            r#"always([+SUPPORT_KEY_INTERWEAVING_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_INTERWEAVING_MESSAGES> true | <+REVOKE_KEY_INTERWEAVING_MESSAGE_EXPOSED_CREDENTIALS> true))"#,
+            r#"always([+PURGE_SUPPORT_KEY_INTERWEAVING_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"#,
             r#"next(<+APPROVE> true)"#,
             r#"next((<+APPROVE> true | [<+REJECT>] true))"#,
             r#"<+WAIT> true until <+APPROVE> true"#,
@@ -8124,6 +8126,18 @@ F2: formula generated_2 {
         ));
         assert!(output.contains(
             "always([+PURGE_SUPPORT_KEY_INTERMIXING_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+        ));
+    }
+
+    #[test]
+    fn synthesis_list_includes_support_key_interweaving_message_secret_leak_prompt_examples() {
+        let output = synthesis_list_text();
+
+        assert!(output.contains(
+            "always([+SUPPORT_KEY_INTERWEAVING_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_INTERWEAVING_MESSAGES> true | <+REVOKE_KEY_INTERWEAVING_MESSAGE_EXPOSED_CREDENTIALS> true))"
+        ));
+        assert!(output.contains(
+            "always([+PURGE_SUPPORT_KEY_INTERWEAVING_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
         ));
     }
 
@@ -21734,6 +21748,23 @@ gfp(X, []((X)) & ([<+ARCHIVE>] true))
         ]);
         let model = modality_lang::formula_synthesis::synthesize_from_formulas(
             "SupportKeyIntermixingMessageSecretLeak",
+            &formulas,
+        );
+
+        verify_synthesized_model(&model, &formulas).unwrap();
+    }
+
+    #[test]
+    fn verify_synthesized_model_accepts_support_key_interweaving_message_secret_leak_prompt_examples()
+    {
+        let formulas = parse_formula_strings(&[
+            "always([+SUPPORT_KEY_INTERWEAVING_MESSAGE_SECRET_LEAKED] true -> (<+PURGE_SUPPORT_KEY_INTERWEAVING_MESSAGES> true | <+REVOKE_KEY_INTERWEAVING_MESSAGE_EXPOSED_CREDENTIALS> true))"
+                .to_string(),
+            "always([+PURGE_SUPPORT_KEY_INTERWEAVING_MESSAGES] true -> eventually(<+NOTIFY_SUPPORT_PRIVACY_OWNER> true))"
+                .to_string(),
+        ]);
+        let model = modality_lang::formula_synthesis::synthesize_from_formulas(
+            "SupportKeyInterweavingMessageSecretLeak",
             &formulas,
         );
 
